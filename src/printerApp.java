3b44ebf41f7d1700ee634c785cf1c95a3063c73d
HANEETADIL <haneeta23000@student.hamk.fi>
2023-11-04 20:09:56 +0200

    adding a comment to division

--- src/printerApp.java
+++ src/printerApp.java
@@ -26,7 +26,7 @@ public class printerApp {
 		//This performs the addition
 		addition = num1 + num2;
 		System.out.println(num1+" + "+ num2 + " = "+ addition);
-		
+		//This performs the division
 		division = num1 / num2;
 		System.out.println(num1+" / "+ num2 + " = "+ division);
 		
