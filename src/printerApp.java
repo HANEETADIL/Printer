6884c2701d060a639cf9bc360a55c5abf0acdded
HANEETADIL <haneeta23000@student.hamk.fi>
2023-11-04 20:02:33 +0200

    Initial commit

---
+++ src/printerApp.java
@@ -0,0 +1,39 @@
+
+public class printerApp {
+
+	public static void main(String[] args) {
+		String creator;
+		int num1;
+		int num2;
+		int multiplication;
+		int addition;
+		double division;
+		int substraction;
+		
+		System.out.println("printer App");
+		System.out.println("This app is made by");
+		creator= "Haneeta";
+		System.out.println("This app is made by "+ creator);
+		
+		num1 = 10;
+		num2 = 2;
+		System.out.println(num1);
+		System.out.printf("The value of num1 is %d\n", num1);
+		System.out.printf("The value of num2 is %d\n", num2);
+		
+		multiplication = num1 * num2;
+		System.out.println(num1+"*"+num2+"="+multiplication);
+		
+		addition = num1 + num2;
+		System.out.println(num1+" + "+ num2 + " = "+ addition);
+		
+		division = num1 / num2;
+		System.out.println(num1+" / "+ num2 + " = "+ division);
+		
+		substraction = num1 - num2;
+		System.out.println(num1+ " - " + num2 + " = " + substraction);
+		
+
+	}
+
+}
